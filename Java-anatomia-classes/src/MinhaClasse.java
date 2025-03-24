1f140d3083286e753e12ce7af0e346f1e6a36d88
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-03-24 18:37:13 -0300

    Anatomia de classe #1

---
+++ Java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse{
+   
+    //                     Parâmetros argumentos
+    public static void main(String[] args) {
+        //inicializa classe
+        //metodos
+         //conteúdo
+        //bloco de código
+        //Imprimindo na tela 
+        System.out.println("Primeira Classe");
+    }
+}//corpo da MinhaClasse
