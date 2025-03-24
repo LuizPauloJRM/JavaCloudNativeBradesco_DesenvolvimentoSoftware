f92566738808332703b98f9f7b707cf3cf359a6e
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-03-24 18:50:25 -0300

    variaveis em java

---
+++ Variaveis/src/Variaveis.java
@@ -0,0 +1,10 @@
+public class Variaveis{
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+        String pais="Brasil";
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(pais);
+    }
+}
